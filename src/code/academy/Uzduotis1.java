192423ed39478d484d5386c98daff752aab9370e
sebpra <sebpra@gmail.com>
2020-08-19 21:39:50 +0300

    Fixed first init

---
+++ src/code/academy/Uzduotis1.java
@@ -0,0 +1,24 @@
+package code.academy;
+
+import java.util.Scanner;
+
+public class Uzduotis1 {
+
+	public static void main(String[] args) {
+		String sakinys = ivesti();
+		int indeksas1 = 0;
+		int indeksas2 = sakinys.length(); // pakeisti
+
+		String z1 = sakinys.substring(indeksas1, indeksas2);
+
+		System.out.println("Pirmas sakinio žodis: " + z1 + ".");
+	}
+
+	private static String ivesti() {
+		System.out.println("ENTER ANY WORD");
+		Scanner scanner = new Scanner(System.in);
+
+		return scanner.next();
+	}
+
+}
